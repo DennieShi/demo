4b0fb548e6f9e7ea63d638e934710caab8f3de62
Daniel <649985113@qq.com>
2021-03-09 13:56:39 +0800

    提交demo01

---
+++ src/test/java/com/yoodb/study/demo01/SpringbootStudyDemo01ApplicationTests.java
@@ -0,0 +1,19 @@
+package com.yoodb.study.demo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@SpringBootTest
+class SpringbootStudyDemo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+
+}
